fea6aeedca0130a57b8523a06e6a341b5e2ca800
Leonardo Amarilio <leoamarilio1606@gmail.com>
2025-10-17 17:23:04 -0300

    Adição de banco de dados e inclusão do Header.

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import react from "react";
+import { FaCommentDots } from "react-icons/fa6";
+
+export default function Header() {
+    return (
+        <div>
+            <p><FaCommentDots /></p>
+        </div>
+    )
+}
